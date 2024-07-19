ef6081f72779afc4d99a55b6eb5be91fd54c2bc8
Maksym Bocharov <Maksym Bocharov>
2024-07-19 16:56:34 +0200

    Use eslint. Add api to get the data. Create "Card" component. Add fonts

---
+++ store/people/entities.ts
@@ -0,0 +1,29 @@
+export interface Character {
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+  url: string;
+  created: string;
+  edited: string;
+}
+
+export interface GetCharactersResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Character[];
+}
+
+export interface GetCharactersPayload {
+  isRefreshing?: boolean;
+}
